4fc6c5aad8f73fa18562af7204edbf0bf73a107b
spal-synaptics <sauryadeep.pal@synaptics.com>
2025-08-01 23:52:58 +0000

    Add common utils

--- demos/embeddings.py
+++ demos/embeddings.py
@@ -7,22 +7,14 @@ from typing import Final
 
 from core.embeddings import TextEmbeddingsAgent
 
+from ._utils import (
+    InferenceStat,
+    add_input_args,
+    configure_logging,
+    format_answer
+)
+
 DEFAULT_QA_FILE: Final = "data/qa_assistant.json"
-
-
-def configure_logging(verbosity: str):
-    level = getattr(logging, verbosity.upper(), None)
-    if not isinstance(level, int):
-        raise ValueError(f"Invalid log level: {verbosity}")
-
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
-    root_logger.handlers.clear()
-    root_logger.addHandler(handler)
 
 
 def run_command(command: str):
@@ -43,9 +35,6 @@ def replace_tool_tokens(answer: str, tools: dict[str, str]):
 
 
 def main():
-    GREEN: Final[str] = "\033[32m"
-    RESET: Final[str] = "\033[0m"
-
     tools_path = Path("data/tools.json")
     with open(tools_path, "r") as f:
         tools = json.load(f)
@@ -58,7 +47,7 @@ def main():
             result = text_agent.answer_query(query)[0]
             answer, similarity, emb_infer_time = result["answer"], result["similarity"], result["infer_time"]
             answer = replace_tool_tokens(answer, tools)
-            print(GREEN + f"Agent: {answer}" + RESET + f" ({emb_infer_time * 1000:.3f} ms, Similarity: {similarity:.6f})")
+            print(format_answer(answer, emb_infer_time, stats=[InferenceStat("Similarity", f"{similarity:.6f}")]))
     except (KeyboardInterrupt, EOFError):
         logger.info("Stopped by user.")
 
@@ -71,28 +60,11 @@ if __name__ == "__main__":
         default=DEFAULT_QA_FILE,
         help="Path to Question-Answer pairs (default: %(default)s)"
     )
-    parser.add_argument(
-        "--logging",
-        type=str,
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        default="INFO",
-        help="Logging verbosity: %(choices)s (default: %(default)s)"
-    )
-    parser.add_argument(
-        "--cpu-only",
-        action="store_true",
-        default=False,
-        help="Use CPU only models"
-    )
-    parser.add_argument(
-        "-j", "--threads",
-        type=int,
-        help="Number of cores to use for CPU execution (default: all)"
-    )
+    add_input_args(parser)
     args = parser.parse_args()
 
-    configure_logging(args.logging)
     logger = logging.getLogger(__name__)
+    configure_logging(logger, args.logging)
     logger.info("Starting demo...")
 
     main()
