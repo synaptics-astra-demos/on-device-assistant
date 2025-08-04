b5b1445d3eef9998878b802c9ed270986e9601b7
spal-synaptics <sauryadeep.pal@synaptics.com>
2025-08-04 06:02:47 +0000

    Add system usage profiling

--- profile/_utils.py
+++ profile/_utils.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Any
 
+from ._sys_usage import SystemProfiler
+
 
 @dataclass
 class ProfilingStat:
@@ -29,6 +31,7 @@ class ProfilerBase(ABC):
         self._model_names = models_names
         self._run_forever = run_forever
         self._logger = logger
+        self._sys_prof = SystemProfiler(interval_ms=25, hist_length=1000)
 
         self._all_stats: dict[str, dict[str, ProfilingStat | dict[str, ProfilingStat]]] = {
             "environment": {
@@ -38,9 +41,10 @@ class ProfilerBase(ABC):
                 model_name: {
                     "n_iters": ProfilingStat("Num inferences", 0),
                     "tot_infer_time": ProfilingStat("Total inference time", 0, "ms"),
-                    "avg_infer_time": ProfilingStat("Average inference time", 0, "ms")
+                    "avg_infer_time": ProfilingStat("Average inference time", 0, "ms"),
+                    "avg_sys_usage": {}
                 } for model_name in self._model_names
-            }
+            },
         }
 
     @abstractmethod
@@ -54,12 +58,21 @@ class ProfilerBase(ABC):
     def _update_env_param(self, param: str, value: ProfilingStat):
         self._all_stats["environment"].update({param: value})
 
+    def _update_sys_usage(self, model_name: str):
+        sys_usage = self._all_stats["infer_stats"][model_name]["avg_sys_usage"]
+        for cpu, usage in self._sys_prof.avg_cpu_usage().items():
+            sys_usage[cpu] = ProfilingStat(cpu.upper(), round(100 * usage, 2), "%")
+        sys_usage["npu"] = ProfilingStat("NPU", round(100 * self._sys_prof.avg_npu_usage(), 2), "%")
+        sys_usage["mem"] = ProfilingStat("RAM", round(self._sys_prof.avg_mem_usage() / 1_048_576, 2), "GB")
+        self._sys_prof.reset()
+
     def profile_models(self, n_iters: int, print_stats: bool = True) -> dict[str, dict]:
 
         def _cleanup_models():
             for model_name in self._model_names:
                 self._cleanup(model_name)
 
+        self._sys_prof.start()
         while True:
             try:
                 for model_name in self._model_names:
@@ -69,6 +82,7 @@ class ProfilerBase(ABC):
                         for _ in range(n_iters):
                             infer_stats["tot_infer_time"].value += self._get_inference_time(model_name)
                             infer_stats["n_iters"].value += 1
+                        self._update_sys_usage(model_name)
                     except Exception as e:
                         self._logger.warning(f"Stopping inference due to error: {e}")
                         break
@@ -77,8 +91,9 @@ class ProfilerBase(ABC):
             except KeyboardInterrupt:
                 print("Stopped by user.")
                 break
-
         _cleanup_models()
+        self._sys_prof.stop()
+    
         for model_name, infer_stats in self._all_stats["infer_stats"].items():
             n_iters = infer_stats["n_iters"].value
             infer_stats["avg_infer_time"].value = round(infer_stats["tot_infer_time"].value / (n_iters or 1)  * 1000, 3)
@@ -88,16 +103,21 @@ class ProfilerBase(ABC):
         return self._all_stats
 
     def print_stats(self):
-
+        SPACER = " " * 4
         print("\n\nProfiling report")
         print("------------------------------------")
         print("Environment:")
-        for value in self._all_stats["environment"].values():
-            print(f"\t{value}")
+        for env_var in self._all_stats["environment"].values():
+            print(SPACER + str(env_var))
         for model_name, infer_stats in self._all_stats["infer_stats"].items():
             print(f"\nStats for '{model_name}':")
-            for value in infer_stats.values():
-                print(f"\t{value}")
+            for stat_name, stat in infer_stats.items():
+                if isinstance(stat, dict) and stat_name == "avg_sys_usage":
+                    print(SPACER + "Average system usage:")
+                    for sys_stat in stat.values():
+                        print(2 * SPACER + str(sys_stat))
+                else:
+                    print(SPACER + str(stat))
         print()
 
 
